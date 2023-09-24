ff09300c630bade1bc33f03093f3cef3b3796d50
hosthabib <hosthabib786@gmail.com>
2023-09-23 19:53:19 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# MyCustomTheme
+This is a WordPress theme named MyCustomTheme.
+
+This is some additional information about the theme.
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme and one of the
+ * two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package mycustometheme
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wordpress Theme</title>
+</head>
+<body>
+    
+</body>
+</html>
